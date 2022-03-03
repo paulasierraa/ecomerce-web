e6644a1145fd16071531f65997f87d5efdc91289
Paula Sierra <PaulaCode01@gmail.com>
2022-03-02 22:31:29 -0500

    Módelos javascript

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,11 @@
 export class User{
-    
+    constructor(id,username,email,password,state,image,id_rol_fk){
+        this.id=id;
+        this.username=username;
+        this.email=email;
+        this.password=password;
+        this.state = state;
+        this.image = image;
+        this.id_rol_fk = id_rol_fk;
+    }
 }
