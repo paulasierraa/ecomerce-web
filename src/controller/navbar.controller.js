fd3885381f8dc3fa194732beffcf9e437f5a78aa
Paula Sierra <PaulaCode01@gmail.com>
2022-03-06 21:45:35 -0500

    Hábilitando botones navbar

--- src/controller/navbar.controller.js
+++ src/controller/navbar.controller.js
@@ -6,12 +6,23 @@ function setPermisos() {
     if (id_rol == "1") {
         const category = divElement.querySelector("#option-category");
         const product = divElement.querySelector("#option-product");
-        const logOut = divElement.querySelector("#option-logout");
-
+        
         category.classList.remove("d-none");
         product.classList.remove("d-none");
-        logOut.classList.remove("d-none")
+        
     }
+    const login = divElement.querySelector("#option-login");
+    const register = divElement.querySelector("#option-register");
+    const logOut = divElement.querySelector("#option-logout");
+    const cart = divElement.querySelector("#option-cart");
+    const history = divElement.querySelector("#option-history");
+
+    login.classList.add("d-none");
+    register.classList.add("d-none");
+    logOut.classList.remove("d-none")
+    cart.classList.remove("d-none")
+    history.classList.remove("d-none")
+
 }
 
 
